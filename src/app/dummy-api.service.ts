fb566a340c15631eeb78f83f134531608d33f24e
Baburao Shelvane <baburao.shelvane@neosofttech.com>
2019-04-30 11:51:21 +0530

    api changes commited

--- src/app/dummy-api.service.ts
+++ src/app/dummy-api.service.ts
@@ -13,15 +13,19 @@ export interface Post {
   providedIn: 'root'
 })
 export class DummyApiService {
-  public url = 'http://jsonplaceholder.typicode.com/posts';
+  public url = 'http://jsonplaceholder.typicode.com';
   
   constructor(private _http: HttpClient) { }
 
   getPosts(): Observable <Post[]>{
-    return this._http.get<Post[]>(this.url).pipe(
+    return this._http.get<Post[]>(`${this.url}/posts`).pipe(
       map( (res:any) => {
           return res;
       })
     );
+  }
+
+  getUsers(): Observable<any> {
+    return  this._http.get(`${this.url}/users`);
   }
 }
